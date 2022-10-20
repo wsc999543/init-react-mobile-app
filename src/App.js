0d0b09a9367c3a32e469868d93721ca7afdf5e7b
阿雨 <809081223@qq.com>
2022-10-20 10:30:14 +0800

    init gee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Toast } from 'antd-mobile'
 
 function App() {
+  const showInfo = () => {
+    Toast.show({ content: 'Hello World' })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button block color="primary" size="large" onClick={showInfo}>
+        Block Button
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
